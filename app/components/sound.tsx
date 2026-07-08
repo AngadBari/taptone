2b4304c15ec0aaaf8e2142da6e8e1a7fec8ce028
AngadBari <angadbari2@gmail.com>
2026-07-09 00:31:50 +0530

    Sounds add

---
+++ app/components/sound.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import React from "react";
+import "tap-tone";
+
+export const Sound = () => {
+  return (
+    <section className="px-4 mt-8 flex flex-wrap gap-3">
+      <SoundBtn data-tone-beep>
+        <CirclePattern className="bg-amber-700" />
+        <SoundName>Beep</SoundName>
+      </SoundBtn>
+
+      <SoundBtn data-tone-success>
+        <CirclePattern className="bg-green-600" />
+        <SoundName>Success</SoundName>
+      </SoundBtn>
+
+      <SoundBtn data-tone-copy>
+        <CirclePattern className="bg-purple-500" />
+        <SoundName>Copy</SoundName>
+      </SoundBtn>
+
+      <SoundBtn data-tone-click>
+        <CirclePattern className="bg-pink-400" />
+        <SoundName>Click</SoundName>
+      </SoundBtn>
+
+
+      <SoundBtn data-tone-error>
+        <CirclePattern className="bg-red-500" />
+        <SoundName>Error</SoundName>
+      </SoundBtn>
+
+     <SoundBtn data-tone-tap>
+        <CirclePattern className="bg-yellow-400" />
+        <SoundName>Tap</SoundName>
+      </SoundBtn>
+
+       <SoundBtn data-tone-press>
+        <CirclePattern className="bg-sky-400" />
+        <SoundName>Press</SoundName>
+      </SoundBtn>
+
+       <SoundBtn data-tone-remove>
+        <CirclePattern className="bg-pink-700" />
+        <SoundName>Remove</SoundName>
+      </SoundBtn>
+
+       <SoundBtn data-tone-droplet>
+        <CirclePattern className="bg-mauve-950" />
+        <SoundName>Droplet</SoundName>
+      </SoundBtn>
+
+       <SoundBtn data-tone-mouse1>
+        <CirclePattern className="bg-cyan-500" />
+        <SoundName>Mouse</SoundName>
+      </SoundBtn>
+
+
+    </section>
+
+    
+  );
+};
+
+const SoundBtn = ({
+  className = "",
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      
+      className={`flex items-center gap-3 border border-neutral-400 rounded-md px-2  hover:border-dashed hover:scale-105 transition-all duration-300 cursor-pointer ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+const CirclePattern = ({
+  className = "",
+}: {
+  className?: string;
+}) => {
+  return (
+    <div
+      className={`w-2 h-2 rounded-full border border-black ${className}`}
+    />
+  );
+};
+
+const SoundName = ({
+  className = "",
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <span className={`font-mono text-[13px] text-neutral-900 ${className}`}>
+      {children}
+    </span>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Link from "next/link";
 import { Inter } from "next/font/google";
+import { motion } from "framer-motion";
+import  {Sound}  from "./components/sound";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -8,18 +11,53 @@ const inter = Inter({
 
 export default function Home() {
   return (
-    <main className="mx-auto min-h-screen w-full max-w-[500px] px-4">
+    <main className="mx-auto min-h-screen w-full max-w-[510px] px-4">
       <header className="mt-10 flex items-center justify-between p-4">
         <h1
-          className={`${inter.className} cursor-pointer text-[18px] transition-colors hover:text-neutral-600`}
+          className={`${inter.className} cursor-pointer text-[18px] transition-colors hover:text-neutral-600 `}
         >
           TapTone
+         
+           <motion.svg
+           data-tone-tap
+              whileHover={{
+                scale: 1.2,
+                rotate: 15,
+               fill: "#F62440",
+              }}
+              animate={{
+                
+                scale: [1, 1.05, 1],
+              }}
+              transition={{
+                duration: 2,
+                repeat: Infinity,
+                ease: "easeInOut",
+              }}
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="inline-block ml-1 align-middle"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M14.504 8.522l-1.758 -4.032a.814 .814 0 0 0 -1.492 0l-1.759 4.032c-.19 .436 -.537 .784 -.973 .973l-4.032 1.759a.814 .814 0 0 0 0 1.492l4.033 1.758c.436 .19 .784 .538 .973 .974l1.759 4.033a.814 .814 0 0 0 1.492 0l1.758 -4.033c.19 -.436 .538 -.784 .974 -.974l4.033 -1.758a.814 .814 0 0 0 0 -1.492l-4.033 -1.759a1.88 1.88 0 0 1 -.974 -.973" />
+              <path d="M3 3l2 2" />
+              <path d="M21 3l-2 2" />
+              <path d="M3 21l2 -2" />
+              <path d="M21 21l-2 -2" />
+            </motion.svg>
         </h1>
- 
+
         <p className="font-mono text-[12px] text-neutral-500">
-          v1.0.0{" "}/{" "}
+          v1.0.0 /{" "}
           <Link
-            href="https://www.npmjs.com/package/taptone"
+            href="https://www.npmjs.com/package/tap-tone?activeTab=readme"
             className="text-neutral-900 "
           >
             npm
@@ -28,9 +66,16 @@ export default function Home() {
       </header>
       <section className="mt-3 px-4">
         <p className={` font-mono text-[13px] text-neutral-500`}>
-  <span className=" text-neutral-900">Beautiful UI interaction sounds that make websites feel physical.</span> no audio files, no dependencies, just a simple and lightweight package.<span className=" text-neutral-900">Only Import and Use it!!</span></p>
-        
+          <span className=" text-neutral-900">
+            Beautiful UI interaction sounds that make websites feel physical.
+          </span>{" "}
+          no audio files, no dependencies, just a simple and lightweight
+          package.
+          <span className=" text-neutral-900">Only Import and Use it!!</span>
+        </p>
       </section>
+      <Sound/>
     </main>
   );
 }
+
